4e905ffef817cca841de51318d54c6717dc877af
Gabriel Leiva <gaboleiva@192.168.1.6>
2024-04-24 11:33:13 -0400

    Se agrega bloques if/else para validar que lleguen parametros correctos

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const { Pool } = require("pg");
 
 const config = {
@@ -21,44 +22,73 @@ const rut = argumentos[2];
 const curso = argumentos[3];
 const nivel = argumentos[4];
 
-// Creamos un nuevo alumno
+// Función asincrona para crear un nuevo alumno.
 const nuevoAlumno =  async ({ nombre, rut, curso, nivel }) => {
 try {
-const res = await pool.query(
-    "INSERT INTO alumnos values ($1, $2, $3, $4) RETURNING *",
-[nombre, rut, curso, nivel]
-);
-console.log(`Alumno ${nombre} del ${curso} agregado con éxito`);
+if (!nombre || !rut || !curso || !nivel) {
+    console.log("No se pudo agregar el alumno.");
+    console.log("Debe ingresar nombre, rut, curso y nivel");
+} 
+else {
+    const res = await pool.query(
+        "INSERT INTO alumnos values ($1, $2, $3, $4) RETURNING *",
+    [nombre, rut, curso, nivel]
+    );
+    console.log(`Alumno ${nombre} del curso ${curso} agregado con éxito:`, res.rows[0]);
+}
     } catch (error) {
         console.log("Mensaje de error: ", error)
     }
-}
+};
 
-// Consultamos un alumno por rut
+// Función asincrona para consultar un alumno pasando como parametro el rut.
 const consultaRut = async ( rut ) => {
     try {
         const res = await pool.query(
-            `SELECT * FROM alumnos WHERE rut = $1`, 
+            "SELECT * FROM alumnos WHERE rut = $1", 
             [rut]
         );
-        console.log("Alumno consultado por rut: ", res.rows[0]);
+        if (!rut) {
+            console.log("Debe ingresar un rut para consultar");
+        }else if (res.rows == 0) {
+            console.log(`El rut ${rut} ingresado no existe`);
+        } else {
+            console.log(`Alumno con rut ${rut}: `, res.rows[0]);
+        }
     } catch (error) {
         console.log("Mensaje de error: ", error);
     }
 };
 
+// Función asincrona para consultar la lista completa de los alumnos.
 const totalAlumnos = async () => {
-    const res = await pool.query(`SELECT * FROM alumnos`);
-    console.log("Alumnos registrados:", res.rows);
+    try {
+        const res = await pool.query(`SELECT * FROM alumnos`);
+        console.log("Alumnos registrados:", res.rows);
+    } catch (error) {
+        console.log("Mensaje de error: ", error)
+    }
   };
 
+// Función asincrona para eliminar Alumno pasandole como parametro el rut 
   const eliminarAlumno = async (rut) => {
-    const res = await pool.query(
-      `DELETE FROM alumnos WHERE rut = $1 RETURNING *`,
-      [rut]
-    );
-    console.log(`El alumno se ha eliminado con éxito`);
-    console.log("Alumno eliminado: ", res.rows[0]);
+      try {
+        const res = await pool.query(
+          `DELETE FROM alumnos WHERE rut = $1 RETURNING *`,
+          [rut]
+        );
+        if (!rut) {
+            console.log("Debe ingresar un rut");
+        } else if (res.rows == 0) {
+            console.log(`El alumno con ${rut} no existe`);
+        }
+        else {
+            console.log(`El alumno con ${rut} eliminado con éxito`);
+            console.log("Alumno eliminado: ", res.rows[0]);
+        }
+    } catch (error) {
+        console.log(`El alumno con ${rut} no existe`);
+    }
 };
 
   // Funcion IIFE que recibe de la linea de comando y llama funciones asincronas internas
@@ -74,7 +104,10 @@ const totalAlumnos = async () => {
       case 'todos':
         totalAlumnos()
         break;
-        case 'eliminar':
+      case 'editar':
+        nuevoAlumno({ nombre, rut, curso, nivel })
+        break;
+      case 'eliminar':
         eliminarAlumno(argumentos[1])
         break;
       default:
