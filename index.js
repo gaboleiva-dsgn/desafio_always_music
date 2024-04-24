0f02bb9541645556d5be2260701d1bd3d12d4b86
Gabriel Leiva <gaboleiva@192.168.1.6>
2024-04-24 12:04:42 -0400

    Se incluyen comando para hacer las pruebas por consola

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const { log } = require("console");
 const { Pool } = require("pg");
 
 const config = {
-  user: "gaboleiva",
-  host: "localhost",
-  password: "P455p0r7",
-  database: "dbalwaysmusic",
-  port: 5432
+    user: "gaboleiva",
+    host: "localhost",
+    password: "P455p0r7",
+    database: "dbalwaysmusic",
+    port: 5432
 };
 
 const pool = new Pool(config);
@@ -23,19 +23,19 @@ const curso = argumentos[3];
 const nivel = argumentos[4];
 
 // Función asincrona para crear un nuevo alumno.
-const nuevoAlumno =  async ({ nombre, rut, curso, nivel }) => {
-try {
-if (!nombre || !rut || !curso || !nivel) {
-    console.log("No se pudo agregar el alumno.");
-    console.log("Debe ingresar nombre, rut, curso y nivel");
-} 
-else {
-    const res = await pool.query(
-        "INSERT INTO alumnos values ($1, $2, $3, $4) RETURNING *",
-    [nombre, rut, curso, nivel]
-    );
-    console.log(`Alumno ${nombre} del curso ${curso} agregado con éxito:`, res.rows[0]);
-}
+const nuevoAlumno = async ({ nombre, rut, curso, nivel }) => {
+    try {
+        if (!nombre || !rut || !curso || !nivel) {
+            console.log("No se pudo agregar el alumno.");
+            console.log("Debe ingresar nombre, rut, curso y nivel");
+        }
+        else {
+            const res = await pool.query(
+                "INSERT INTO alumnos values ($1, $2, $3, $4) RETURNING *",
+                [nombre, rut, curso, nivel]
+            );
+            console.log(`Alumno ${nombre} del curso ${curso} agregado con éxito:`, res.rows[0]);
+        }
     } catch (error) {
         console.log("Mensaje de error: ", error)
     }
@@ -57,20 +57,19 @@ const editarAlumno = async ({ nombre, rut, curso, nivel }) => {
             console.log("El alumno editado: ", res.rows[0]);
         }
     } catch (error) {
-    // manejo general de errores
-    manejoErrores(error, pool, 'estudiantes');
-}
+        console.log("Mensaje de error: ", error)
+    }
 }
 // Función asincrona para consultar un alumno pasando como parametro el rut.
-const consultaRut = async ( rut ) => {
+const consultaRut = async (rut) => {
     try {
         const res = await pool.query(
-            "SELECT * FROM alumnos WHERE rut = $1", 
+            "SELECT * FROM alumnos WHERE rut = $1",
             [rut]
         );
         if (!rut) {
             console.log("Debe ingresar un rut para consultar");
-        }else if (res.rows == 0) {
+        } else if (res.rows == 0) {
             console.log(`El rut ${rut} ingresado no existe`);
         } else {
             console.log(`Alumno con rut ${rut}: `, res.rows[0]);
@@ -88,14 +87,14 @@ const totalAlumnos = async () => {
     } catch (error) {
         console.log("Mensaje de error: ", error)
     }
-  };
+};
 
 // Función asincrona para eliminar Alumno pasandole como parametro el rut 
-  const eliminarAlumno = async (rut) => {
-      try {
+const eliminarAlumno = async (rut) => {
+    try {
         const res = await pool.query(
-          `DELETE FROM alumnos WHERE rut = $1 RETURNING *`,
-          [rut]
+            `DELETE FROM alumnos WHERE rut = $1 RETURNING *`,
+            [rut]
         );
         if (!rut) {
             console.log("Debe ingresar un rut");
@@ -111,32 +110,49 @@ const totalAlumnos = async () => {
     }
 };
 
-  // Funcion IIFE que recibe de la linea de comando y llama funciones asincronas internas
+// Funcion IIFE que recibe de la linea de comando y llama funciones asincronas internas
 (async () => {
     // recibir funciones y campos de la linea de comando
     switch (funcion) {
-      case 'agregar':
-        nuevoAlumno({ nombre, rut, curso, nivel })
-        break;
-      case 'editar':
-        editarAlumno({ nombre, rut, curso, nivel })
-        break;
-      case 'rut':
-        consultaRut(argumentos[1])
-        break;
-      case 'todos':
-        totalAlumnos()
-        break;
-      case 'editar':
-        nuevoAlumno({ nombre, rut, curso, nivel })
-        break;
-      case 'eliminar':
-        eliminarAlumno(argumentos[1])
-        break;
-      default:
-        console.log("Funcion: " + funcion + "no es valida")
-        break;
+        case 'agregar':
+            nuevoAlumno({ nombre, rut, curso, nivel })
+            break;
+        case 'editar':
+            editarAlumno({ nombre, rut, curso, nivel })
+            break;
+        case 'rut':
+            consultaRut(argumentos[1])
+            break;
+        case 'todos':
+            totalAlumnos()
+            break;
+        case 'editar':
+            nuevoAlumno({ nombre, rut, curso, nivel })
+            break;
+        case 'eliminar':
+            eliminarAlumno(argumentos[1])
+            break;
+        default:
+            console.log("Funcion: " + funcion + "no es valida")
+            break;
     }
-    
+
     pool.end()
-  })()
+})()
+
+// Comandos para probar por cosola
+//
+// Ingresar nuevo alumnos: 
+// node index agregar Claudia 12.345.678-9 G-72 Experta
+//
+// Consultar todos:
+// node index todos
+//
+// Consultar por rut:
+// node index rut 12.345.678-9
+//
+// Editar alumno por rut:
+// node index editar Gilia 12.345.678-9 E-70 Avanzada
+//
+// Eliminar alumno por rut:
+// node index eliminar 12.345.678-9
