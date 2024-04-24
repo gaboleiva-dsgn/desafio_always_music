1294a89481de80ed45c9d1e9cf332c57821e02c1
Gabriel Leiva <gaboleiva@192.168.1.6>
2024-04-24 00:22:12 -0400

    editamos instrucciones para crear la base de datos y la tabla

--- comand.sql
+++ comand.sql
@@ -1,3 +1,7 @@
+-- Creamos la base de datos
+CREATE DATABASE dbalwaysmusic;
+
+-- Creamos la tabla alumnos
 CREATE TABLE alumnos (
     nombre VARCHAR(30) NOT NULL,
     rut VARCHAR(30) PRIMARY KEY,
